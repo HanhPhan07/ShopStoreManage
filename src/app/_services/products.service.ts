892eee42a3ae78c6f339807e00bef50159a14239
Hanh Phan <honghanhpretty20@gmail.com>
2019-12-08 17:17:45 +0700

    test

---
+++ src/app/_services/products.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams } from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {environment} from 'src/environments/environment';
+
+import {} from '../_models/sanpham';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+constructor() { }
+
+}
